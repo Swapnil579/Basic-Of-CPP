c9cfa552871d7ff5df9e908749748ce4e2d3b9b5
Swapnil579 <singhswapnil579@gmail.com>
2023-11-15 10:53:48 +0530

    Basic of C++

---
+++ Things to Know in C++/FirstProgram.cpp
@@ -0,0 +1,8 @@
+// #include<iostream> is used for input and output operations
+#include<iostream>
+using namespace std;
+// After including the necessary libraries, you declare the main function using int main() { /* Your code here */ return 0; }. This serves as the entry point for your program.
+int main(){
+  cout<<"NAMASTE INDIA";
+return 0;
+}
